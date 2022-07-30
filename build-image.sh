af6529289358bf89f9d5a01eaf1ff6d8d319b7e3
anurag.ag <anurag.ag@media.net>
2022-07-30 08:40:42 +0530

    Merges build and push images

--- build-image.sh
+++ build-image.sh
@@ -4,3 +4,5 @@ source ./set-env.sh
 
 $MVN clean integration-test package docker:build -Ddocker.pull.registry=docker.io $@
 $MVN docker:tag -Ddocker.image.tag=$DOCKER_TAG -Ddocker.image.repo=$DOCKER_REGISTRY/$DOCKER_REPOSITORY
+
+docker push $DOCKER_REGISTRY/$DOCKER_REPOSITORY:$DOCKER_TAG
--- push-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-source ./set-env.sh
-
-docker push $DOCKER_REGISTRY/$DOCKER_REPOSITORY:$DOCKER_TAG
